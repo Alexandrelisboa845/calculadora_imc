057aa0116353c59ca95db62e49407dbe630f09f0
Alexandre Lisboa <Alexandrelisboa845@gmail.com>
2023-03-13 08:07:06 +0100

    fix calculador imc

--- bin/calculadora_imc.dart
+++ bin/calculadora_imc.dart
@@ -1,10 +1,13 @@
 import 'package:calculadora_imc/calculadora_imc.dart' as calculadora_imc;
-import 'package:calculadora_imc/Class/Pessoa.dart' ;
+import 'package:calculadora_imc/Class/Pessoa.dart';
 import 'dart:io';
-void main(List<String> arguments) { 
 
-  var p1 = new Pessoa();
-  
+void main(List<String> arguments) {
+  var p1 = Pessoa();
+  var lernome = stdin.readLineSync();
+  p1.setNome(lernome);
+  p1.setPeso(65.0);
+  p1.setAltua(0);
+  p1.ResultImc();
   print('Hello world: ${calculadora_imc.calculate()}!');
-
 }
--- lib/Class/Pessoa.dart
+++ lib/Class/Pessoa.dart
@@ -1,29 +1,48 @@
-
 class Pessoa {
+  String? _nome = "";
+  double _peso = 0;
+  double _altura = 0.00;
 
-  String _nome="";
-  double _peso=0;
-  double _altura=0.00;
-
-  void setNome(String nome){
-   _nome = nome;
+  void setNome(String? nome) {
+    _nome = nome;
   }
 
-   String getNome()=>this._nome;
+  String? getNome() => _nome;
 
-  void setPeso(double peso){
-     _peso = peso;
-  }
-    double getPeso()=>this._peso;
-
-  void setAltua(double altura){
-      _altura = altura;
-  }
-    double getaltura()=>this._altura;
-
-  void IMC (_peso,_altura)
-  {
-      double imc = this._peso/(this._altura * this._altura );
+  void setPeso(double peso) {
+    _peso = peso;
   }
 
+  double getPeso() => _peso;
+
+  void setAltua(double altura) {
+    _altura = altura;
+  }
+
+  double getaltura() => _altura;
+
+  void ResultImc() {
+    try {
+      double imc = _peso / (_altura * _altura);
+      if (imc < 16) {
+        print("Magreza grave");
+      } else if (imc == 16 || imc < 17) {
+        print("Magreza moderada");
+      } else if (imc == 17 || imc < 18.5) {
+        print("Magreza leve");
+      } else if (imc == 18.5 || imc < 25) {
+        print("Saudável");
+      } else if (imc == 25 || imc < 30) {
+        print("Sobrepeso");
+      } else if (imc == 30 || imc < 35) {
+        print("Obesidade Grau I");
+      } else if (imc == 35 || imc < 40) {
+         print("Obesidade Grau II (severa)");
+      } else if (imc >= 40) {
+         print("Obesidade Grau III (mórbida)");
+      }
+    } catch (e) {
+      print("Numero invalido");
+    }
+  }
 }
